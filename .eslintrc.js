1a20b5c856ca41dc537d110522dfb79588961b39
yankvasya <yankvasya@gmail.com>
2021-12-29 00:21:19 +0300

    chore(eslint): added new rules in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,17 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "import/prefer-default-export": 'off',
+    "max-len": "off",
+    "vue/max-len": [
+      "error",
+      {
+        "code": 120,
+        "template": 9000,
+        "ignoreTemplateLiterals": true,
+        "ignoreUrls": true,
+        "ignoreStrings": true
+      }
+    ]
   },
 };
