11fbaadf143088607684a7a731f03aa99394eaf4
yankvasya <yankvasya@gmail.com>
2021-12-29 18:21:34 +0300

    refactor(listBox): split into component

---
+++ src/components/listBox/listBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="colors"
+    v-for="l in 5"
+        :key="l"
+  >
+    List {{l}}
+    <div class="colors__list"
+         v-for="a in 5"
+         :key="a"
+    >
+      <color
+        v-for="c in 50"
+        :key="c"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import color from '../color/color.vue';
+
+export default {
+  name: 'listBox',
+  components: {
+    color,
+  },
+};
+</script>
+
+<style lang="scss" src="./style.scss" scoped />
