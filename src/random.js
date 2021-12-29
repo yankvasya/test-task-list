1b53b094a4c5fb66bae24e3383b6405910541bf9
yankvasya <yankvasya@gmail.com>
2021-12-29 22:54:22 +0300

    fix: fix randomColor function

--- src/random.js
+++ src/random.js
@@ -1,6 +1,10 @@
 export const randomNumber = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
 
-export const randomColor = () => randomNumber(0, 16777215).toString(16);
+export const randomColor = () => {
+  const res = randomNumber(0, 16777215).toString(16);
+
+  return res.length === 6 ? res : randomColor();
+};
 
 export const createRandomLists = () => {
   let lists = Array.from([{}, {}, {}], (x) => x);
