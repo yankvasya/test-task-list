d796b1c9f003e6ae693343887b9329ef95a4cdc6
yankvasya <yankvasya@gmail.com>
2021-12-29 18:25:46 +0300

    feat(App): create and push list data to localStorage at start

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,36 @@
 <script>
 import Menu from './components/Menu/Menu.vue';
 import Display from './components/Display/Display.vue';
+import { createRandomLists } from './random';
 
 export default {
   name: 'App',
   components: {
     Menu,
     Display,
+  },
+  data() {
+    return {
+      lists: null,
+    };
+  },
+  methods: {
+    checkLocalStorage() {
+      if (!localStorage.getItem('lists')) {
+        const lists = createRandomLists();
+        this.setLocalStorage(lists);
+      }
+
+      this.lists = JSON.parse(localStorage.getItem('lists'));
+    },
+    setLocalStorage(lists) {
+      localStorage.setItem('lists', JSON.stringify(lists));
+    },
+  },
+  created() {
+    this.checkLocalStorage();
+
+    console.log(this.lists);
   },
 };
 </script>
