0fef4c17f8cc0e910b91232693ec492b0fcacbca
yankvasya <yankvasya@gmail.com>
2021-12-29 18:23:43 +0300

    refactor(list): split into component

--- src/components/list/list.vue
+++ src/components/list/list.vue
@@ -1,20 +1,49 @@
 <template>
   <div class="list">
     <div class="list__title">
-      <slot name="arrow" />
-      <slot name="checkbox" />
+      <a href="#" @click="arrowClick">
+        <icon
+          name="arrow"
+          :class="['arrow ', {'closed': !isArrowOpen}]"
+        />
+      </a>
+      <checkbox />
       <div>List {{number}}</div>
     </div>
-    <slot name="item" />
+    <div v-if="isArrowOpen">
+      <item
+        v-for="i in 4"
+        :key="i"
+        :number="i"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import icon from '../../icons/icon.vue';
+import checkbox from '../checkbox/checkbox.vue';
+import item from '../item/item.vue';
 
 export default {
   name: 'list',
   props: {
     number: Number,
+  },
+  components: {
+    icon,
+    checkbox,
+    item,
+  },
+  data() {
+    return {
+      isArrowOpen: true,
+    };
+  },
+  methods: {
+    arrowClick() {
+      this.isArrowOpen = !this.isArrowOpen;
+    },
   },
 };
 </script>
