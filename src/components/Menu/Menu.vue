61b8733585b34f65dc1bd381eb39ab55c7ea9cb0
yankvasya <yankvasya@gmail.com>
2021-12-29 18:24:15 +0300

    refactor(Menu): replaced with list component

--- src/components/Menu/Menu.vue
+++ src/components/Menu/Menu.vue
@@ -5,34 +5,17 @@
       :key="l"
       :number="l"
     >
-      <template #arrow>
-        <icon name="arrow" class="arrow" />
-        <checkbox />
-      </template>
-      <template #item>
-        <item
-          v-for="i in 4"
-          :key="i"
-          :number="i"
-        />
-      </template>
     </list>
   </div>
 </template>
 
 <script>
 import list from '../list/list.vue';
-import item from '../item/item.vue';
-import icon from '../../icons/icon.vue';
-import checkbox from '../checkbox/checkbox.vue';
 
 export default {
   name: 'Menu',
   components: {
     list,
-    item,
-    icon,
-    checkbox,
   },
 };
 </script>
