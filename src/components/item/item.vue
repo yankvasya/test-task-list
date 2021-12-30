05aa0aa1a347892cea341e63af55c623eb7ea67a
yankvasya <yankvasya@gmail.com>
2021-12-30 15:33:23 +0300

    feat(item): added new func, checkbox change visible lists & value change

--- src/components/item/item.vue
+++ src/components/item/item.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="item">
     <div class="item__element">
-      <input type="checkbox" v-model="checked" />
+      <input type="checkbox" v-model="checked" @click="checkboxClick" />
       {{number && `Item ${number}`}}
     </div>
 
     <div class="item__info">
-      <input type="number" class="count" placeholder="0" :value="value" min="0" max="100" />
+      <input
+        type="number"
+        class="count"
+        placeholder="0"
+        min="0"
+        max="100"
+        maxlength="100"
+        :value="value"
+        ref="input"
+        @input="valueChanged" />
       <input type="color" class="color" :value="color">
     </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   name: 'item',
   props: {
@@ -25,6 +36,20 @@ export default {
     return {
       checked: this.check || false,
     };
+  },
+  methods: {
+    ...mapActions({
+      changeColorCount: 'colors/changeColorCount',
+    }),
+    checkboxClick() {
+      this.$emit('checkboxChanged', this.color);
+      // console.log(!this.checked ? 'Включилось' : 'отключилось');
+    },
+    valueChanged() {
+      const amount = this.$refs.input.value * 1;
+      const data = { color: this.color, amount };
+      this.changeColorCount(data);
+    },
   },
 };
 </script>
