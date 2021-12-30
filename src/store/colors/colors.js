fdc96f0be13475e5bc1366de55247bcc98467242
yankvasya <yankvasya@gmail.com>
2021-12-30 15:28:23 +0300

    feat(store): created functions to change: visible, amount

--- src/store/colors/colors.js
+++ src/store/colors/colors.js
@@ -1,51 +1,44 @@
 export default {
   namespaced: true,
-  state: {
-    colors: [
-      {
-        items:
-          [
-            { color: '#1de9b2', num: 27, checked: true },
-            { color: '#2fadf6', num: 33, checked: false },
-            { color: '#ede752', num: 46, checked: true },
-            { color: '#c768f5', num: 13, checked: true },
-          ],
-      },
-      {
-        items:
-          [
-            { color: '#af801a', num: 11, checked: false },
-            { color: '#c5b0f5', num: 0, checked: true },
-            { color: '#abb9a8', num: 37, checked: true },
-            { color: '#6598ae', num: 18, checked: true },
-            { color: '#9ed450', num: 30, checked: false },
-            { color: '#5ad9f3', num: 42, checked: true },
-          ],
-      },
-      {
-        items:
-          [
-            { color: '#501bec', num: 29, checked: false },
-            { color: '#afae88', num: 11, checked: false },
-            { color: '#96bd4d', num: 46, checked: true },
-            { color: '#e261bd', num: 13, checked: true },
-            { color: '#86bdf8', num: 12, checked: false },
-            { color: '#133ff0', num: 22, checked: true },
-            { color: '#467913', num: 41, checked: false },
-            { color: '#42d0d5', num: 29, checked: false },
-          ],
-      },
-    ],
-  },
+  state: {},
   getters: {},
   mutations: {
     SET_COLORS(state, payload) {
       this.state.colors = payload;
     },
+    CHANGE_COLOR_AMOUNT(state, { color, amount = null }) {
+      this.state.colors = this.state.colors.map((e) => {
+        e.items.map((i) => {
+          if (i.color === color) i.num = amount || i.num - 1;
+
+          return i;
+        });
+
+        return e;
+      });
+    },
+    CHANGE_VISIBLE(state, payload) {
+      this.state.colors = this.state.colors.map((e) => {
+        e.items.map((i) => {
+          if (i.color === payload) i.checked = !i.checked;
+
+          return i;
+        });
+
+        return e;
+      });
+    },
   },
   actions: {
     setDefaultColors({ commit }, colors) {
       commit('SET_COLORS', colors);
+    },
+    changeColorCount({ commit }, data) {
+      const payload = { color: data.color, amount: data?.amount };
+      commit('CHANGE_COLOR_AMOUNT', payload);
+    },
+    changeVisible({ commit }, color) {
+      commit('CHANGE_VISIBLE', color);
     },
   },
 };
